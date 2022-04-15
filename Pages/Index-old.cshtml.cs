78b5a2c8a83ace55d3b636ca7fda829d77c7f68c
tohoangphucdn <tohoangphucdn@yahoo.com>
2022-04-15 10:25:09 -0700

    Saving demo index page as Index-old.cshtml

---
+++ Pages/Index-old.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CupOfSugar.Pages
+{
+    /// <summary>
+    /// Arielle Wilson
+    /// </summary>
+    /// <summary>
+    /// Ana Carolina de Souza Mendes
+    /// </summary>
+    /// <summary>
+    /// Phuc To
+    /// </summary>
+    /// <summary>
+    /// Ananya Reddy
+    /// </summary>
+    /*public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }*/    
+}
