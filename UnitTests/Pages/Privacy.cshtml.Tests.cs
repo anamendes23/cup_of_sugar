6b0f98fe3b4ecff5e88f2b0aed6b5cac68cbfed6
tohoangphucdn <tohoangphucdn@yahoo.com>
2022-04-24 13:31:52 -0700

    Added Error.cshtml.Tests.cs in Pages folder

---
+++ UnitTests/Pages/Privacy.cshtml.Tests.cs
@@ -0,0 +1,47 @@
+using Microsoft.Extensions.Logging;
+
+using NUnit.Framework;
+
+using Moq;
+
+using CupOfSugar.Pages;
+
+namespace UnitTests.Pages.Privacy
+{
+    public class PrivacyTests
+    {
+        #region TestSetup
+        public static PrivacyModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<PrivacyModel>>();
+
+            pageModel = new PrivacyModel(MockLoggerDirect)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnGet
+    }
+}
