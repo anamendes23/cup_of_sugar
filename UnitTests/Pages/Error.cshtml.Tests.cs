bad775112a6884a724514c60d8fe621407dd37ce
Arielle Wilson <jwarielle@gmail.com>
2022-04-24 13:26:32 -0700

    Added Error.cshtml.Tests.cs in Pages folder

---
+++ UnitTests/Pages/Error.cshtml.Tests.cs
@@ -0,0 +1,69 @@
+using System.Diagnostics;
+
+using Microsoft.Extensions.Logging;
+
+using NUnit.Framework;
+
+using Moq;
+
+using CupOfSugar.Pages;
+
+namespace UnitTests.Pages.Error
+{
+    public class ErrorTests
+    {
+        #region TestSetup
+        public static ErrorModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<ErrorModel>>();
+
+            pageModel = new ErrorModel(MockLoggerDirect)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
+        {
+            // Arrange
+
+            Activity activity = new Activity("activity");
+            activity.Start();
+
+            // Act
+            pageModel.OnGet();
+
+            // Reset
+            activity.Stop();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(activity.Id, pageModel.RequestId);
+        }
+
+        [Test]
+        public void OnGet_InValid_Activity_Null_Should_Return_TraceIdentifier()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual("trace", pageModel.RequestId);
+            Assert.AreEqual(true, pageModel.ShowRequestId);
+        }
+        #endregion OnGet
+    }
+}
