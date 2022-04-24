694c020ed8d6fa04fdfc2bb454d9fc134bcd793a
anamendes23 <anamendes23@gmail.com>
2022-04-24 12:58:32 -0700

    Added TestFixture.cs in UnitTests project

---
+++ UnitTests/TestFixture.cs
@@ -0,0 +1,52 @@
+using System.IO;
+
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [SetUpFixture]
+    public class TestFixture
+    {
+        // Path to the Web Root
+        public static string DataWebRootPath = "./wwwroot";
+
+        // Path to the data folder for the content
+        public static string DataContentRootPath = "./data/";
+
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
+        {
+            // Run this code once when the test harness starts up.
+
+            // This will copy over the latest version of the database files
+
+            var DataWebPath = "../../../../CupOfSugar/bin/Debug/net5.0/wwwroot/data";
+            var DataUTDirectory = "wwwroot";
+            var DataUTPath = DataUTDirectory + "/data";
+
+            // Delete the Detination folder
+            if (Directory.Exists(DataUTDirectory))
+            {
+                Directory.Delete(DataUTDirectory, true);
+            }
+            
+            // Make the directory
+            Directory.CreateDirectory(DataUTPath);
+
+            // Copy over all data files
+            var filePaths = Directory.GetFiles(DataWebPath);
+            foreach (var filename in filePaths)
+            {
+                string OriginalFilePathName = filename.ToString();
+                var newFilePathName = OriginalFilePathName.Replace(DataWebPath, DataUTPath);
+
+                File.Copy(OriginalFilePathName, newFilePathName);
+            }
+        }
+
+        [OneTimeTearDown]
+        public void RunAfterAnyTests()
+        {
+        }
+    }
+}
