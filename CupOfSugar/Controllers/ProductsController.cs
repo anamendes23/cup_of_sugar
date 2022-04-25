3857075b8e5f3da65bd809c406a15263c0e4c9aa
Ananya Reddy Komatireddy <ananyareddy2435@gmail.com>
2022-04-25 10:52:59 -0700

    Added class documentation and documentation to the constructor, Product Service getter, Get Products, Update Rating methods to ProductsController.cs

--- CupOfSugar/Controllers/ProductsController.cs
+++ CupOfSugar/Controllers/ProductsController.cs
@@ -9,23 +9,47 @@ using CupOfSugar.WebSite.Models;
 
 namespace CupOfSugar.WebSite.Controllers
 {
+    /// <summary>
+    /// ProductsController
+    /// Deals with CRUDi requests made to website
+    /// </summary>
     [Route("[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        /// <summary>
+        /// Constructor to the ProductsController class
+        /// It initializes the productService object
+        /// </summary>
+        /// <param name="productService"></param>
         public ProductsController(JsonFileProductService productService)
         {
             this.ProductService = productService;
         }
 
+        /// <summary>
+        /// Getter method for the ProductService
+        /// </summary>
         public JsonFileProductService ProductService { get; }
 
+        /// <summary>
+        ///This is the Read method in CRUDi 
+        ///Uses HttpGet to get all the products
+        /// </summary>
+        /// <returns>List of all products</returns>
         [HttpGet]
         public IEnumerable<Product> Get()
         {
             return ProductService.GetProducts();
         }
 
+        /// <summary>
+        /// CRUdi - Update method
+        /// This method updates the rating
+        /// </summary>
+        /// <param name="ProductId"></param>
+        /// <param name="Rating"></param>
+        /// <returns>Ok Status</returns>
         //[HttpPatch] "[FromBody]"
         [Route("Rate")]
         [HttpGet]
