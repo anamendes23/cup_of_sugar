8dca899340e780545f40bc72b844c6c50bf39546
Ananya Reddy Komatireddy <ananyareddy2435@gmail.com>
2022-04-25 11:38:56 -0700

    Added documentation to class, attributes, JsonFileName, GetProducts, AddRating methods along with some inline comments to JsonFileProductService.cs

--- CupOfSugar/Services/JsonFileProductService.cs
+++ CupOfSugar/Services/JsonFileProductService.cs
@@ -7,24 +7,42 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace CupOfSugar.WebSite.Services
 {
+    /// <summary>
+    /// JsonFileProductService
+    /// Accesses, adds and modifies products in Products.json
+    /// </summary>
     public class JsonFileProductService
     {
+        /// <summary>
+        /// JsonFileProductService constructor
+        /// Initializes the webHostEnvironment object
+        /// </summary>
+        /// <param name="webHostEnvironment"></param>
         public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
         {
             WebHostEnvironment = webHostEnvironment;
         }
 
-        public IWebHostEnvironment WebHostEnvironment { get; }
+        public IWebHostEnvironment WebHostEnvironment { get; } //Provides info about application's webhosting env
 
+        /// <summary>
+        /// method that returns the path of the products.json file
+        /// </summary>
         private string JsonFileName
         {
-            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+            //returns path string
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); } 
         }
 
+        /// <summary>
+        /// Gets all products from Products.json
+        /// </summary>
+        /// <returns>IEnumerable list of products</returns>
         public IEnumerable<Product> GetProducts()
         {
             using (var jsonFileReader = File.OpenText(JsonFileName))
             {
+                //converts into a list of products
                 return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
                     new JsonSerializerOptions
                     {
@@ -33,15 +51,23 @@ namespace CupOfSugar.WebSite.Services
             }
         }
 
+        /// <summary>
+        /// Updating a product rating in Products.json
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <param name="rating"></param>
         public void AddRating(string productId, int rating)
         {
-            var products = GetProducts();
-            var query = products.First(x => x.Id == productId);
+            var products = GetProducts(); //stores all products
+            var query = products.First(x => x.Id == productId); //Gets the first product with a matching Id
 
+            //adds a new int array when there are no ratings for a product
             if(query.Ratings == null)
             {
                 query.Ratings = new int[] { rating };
             }
+
+            //adds rating to int rating array
             else
             {
                 var ratings = query.Ratings.ToList();
