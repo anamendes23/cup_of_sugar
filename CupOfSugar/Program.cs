ef1f053e0dd10b051d01b4fbbe0f44f816a2f6f6
Ananya Reddy Komatireddy <ananyareddy2435@gmail.com>
2022-04-25 11:44:38 -0700

    Added documentation to the class, main method, and CreateHostBuilder method to Program.cs

--- CupOfSugar/Program.cs
+++ CupOfSugar/Program.cs
@@ -9,13 +9,27 @@ using System.Threading.Tasks;
 
 namespace CupOfSugar.WebSite
 {
+    /// <summary>
+    /// Program class
+    /// Builds project
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main method
+        /// starts the website
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Returns the startup type to be used by the webhost
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns>startup type</returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
