795c868a6b3fd4bd625db564d27e3bb5fd09d1ce
Ananya Reddy Komatireddy <ananyareddy2435@gmail.com>
2022-04-25 11:23:20 -0700

    Added class documentation, OnGet method documentation to Lend.cshtml.cs

--- CupOfSugar/Pages/Lend.cshtml.cs
+++ CupOfSugar/Pages/Lend.cshtml.cs
@@ -8,8 +8,15 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace CupOfSugar.Pages
 {
    
+    /// <summary>
+    /// LendModel class
+    /// Will be developed further to Lend page - Create in CRUDi associated with Lend form
+    /// </summary>
     public class LendModel : PageModel
     {
+        /// <summary>
+        /// placeholder for OnGet 
+        /// </summary>
         public void OnGet()
         {
         }
