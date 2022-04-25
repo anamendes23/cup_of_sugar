7d3ccc5e9129637e33f7198db77d667f953ae1b1
Ananya Reddy Komatireddy <ananyareddy2435@gmail.com>
2022-04-25 11:28:58 -0700

    Added documentation to class, attribute, constructor and OnGet method to Privacy.cshtml.cs

--- CupOfSugar/Pages/Privacy.cshtml.cs
+++ CupOfSugar/Pages/Privacy.cshtml.cs
@@ -8,15 +8,27 @@ using System.Threading.Tasks;
 
 namespace CupOfSugar.Pages
 {
+    /// <summary>
+    /// PrivacyModel class
+    /// For privacy page
+    /// </summary>
     public class PrivacyModel : PageModel
     {
-        private readonly ILogger<PrivacyModel> _logger;
+        private readonly ILogger<PrivacyModel> _logger; //readonly ILogger object that allows logging
 
+        /// <summary>
+        /// PrivacyModel constructor
+        /// initializes logger
+        /// </summary>
+        /// <param name="logger"></param>
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// placeholder for OnGet method
+        /// </summary>
         public void OnGet()
         {
         }
