551bf2b3c23a7bbbddfaf1dbb3be677df7a88ad0
Ananya Reddy Komatireddy <ananyareddy2435@gmail.com>
2022-04-25 11:01:26 -0700

    Added class documentation, documented attributes and the ToString(), to Product.cs

--- CupOfSugar/Models/Product.cs
+++ CupOfSugar/Models/Product.cs
@@ -7,23 +7,32 @@ using System.Threading.Tasks;
 
 namespace CupOfSugar.WebSite.Models
 {
+    /// <summary>
+    /// Product class
+    /// Product model, specifies the attributes of a product
+    /// </summary>
     public class Product
     {
-        public string Id { get; set; }
 
-        public string Maker { get; set; }
+        public string Id { get; set; } //string Id of product
+
+        public string Maker { get; set; } //string Maker of product
 
         [JsonPropertyName("img")]
-        public string Image { get; set; }
+        public string Image { get; set; } //string url of Image of the product
 
-        public string Url { get; set; }
+        public string Url { get; set; } //string Url to the product
 
-        public string Title { get; set; }
+        public string Title { get; set; } //string Title of product
 
-        public string Description { get; set; }
+        public string Description { get; set; } //string Description of product
 
-        public int[] Ratings { get; set; }
+        public int[] Ratings { get; set; } //integer array of ratings of product
 
+        /// <summary>
+        /// Returns string representation of product object
+        /// </summary>
+        /// <returns>string representation</returns>
         public override string ToString() => JsonSerializer.Serialize<Product>(this);
     }
 }
