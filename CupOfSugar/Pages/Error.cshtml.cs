71cfd208f465da62b2b0aa5ac02c5edf82e1b32b
Ananya Reddy Komatireddy <ananyareddy2435@gmail.com>
2022-04-25 11:09:28 -0700

    Added class documentation, attribute documentation, and documentation to ShowRequestId, constructor and OnGet methods to Error.cshtml.cs

--- CupOfSugar/Pages/Error.cshtml.cs
+++ CupOfSugar/Pages/Error.cshtml.cs
@@ -9,20 +9,37 @@ using System.Threading.Tasks;
 
 namespace CupOfSugar.Pages
 {
+    /// <summary>
+    /// Error Page class
+    /// Defines the errors for bad website requests
+    /// </summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
-        public string RequestId { get; set; }
 
+        public string RequestId { get; set; } //string RequestId is the id of request object
+
+        /// <summary>
+        /// Takes RequestId as an input
+        /// And returns true if it is a valid request
+        /// </summary>
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-        private readonly ILogger<ErrorModel> _logger;
+        private readonly ILogger<ErrorModel> _logger; //ILogger object for error logging
 
+        /// <summary>
+        /// Constructor to the ErrorModel
+        /// Initializes the logger
+        /// </summary>
+        /// <param name="logger"></param>
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Gets the Id of the http request
+        /// </summary>
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
