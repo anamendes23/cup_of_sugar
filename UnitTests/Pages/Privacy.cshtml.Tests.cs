9329c3af3348da350fcc1a9ef7d38c4168d281a0
Ananya Reddy Komatireddy <ananyareddy2435@gmail.com>
2022-04-25 20:19:48 -0700

    Added the documentation to the class and the Unit test methods to Privacy.cshtml.Tests.cs

--- UnitTests/Pages/Privacy.cshtml.Tests.cs
+++ UnitTests/Pages/Privacy.cshtml.Tests.cs
@@ -8,11 +8,17 @@ using CupOfSugar.Pages;
 
 namespace UnitTests.Pages.Privacy
 {
+    /// <summary>
+    /// Unit tests class for Privacy page
+    /// </summary>
     public class PrivacyTests
     {
         #region TestSetup
         public static PrivacyModel pageModel;
 
+        /// <summary>
+        /// Initializes an object of the PrivacyModel
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -28,6 +34,10 @@ namespace UnitTests.Pages.Privacy
         #endregion TestSetup
 
         #region OnGet
+
+        /// <summary>
+        /// Unit test to check if the Privacy page is being rendered correctly
+        /// </summary>
         [Test]
         public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
         {
