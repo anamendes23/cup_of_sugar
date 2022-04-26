0dd55c1bef81dd28bc642fb2d0f7b359f6030074
Ananya Reddy Komatireddy <ananyareddy2435@gmail.com>
2022-04-25 20:29:53 -0700

    Documented class and unit tests for the startup page to StartupTests.cs

--- UnitTests/StartupTests.cs
+++ UnitTests/StartupTests.cs
@@ -5,10 +5,14 @@ using NUnit.Framework;
 
 namespace UnitTests.Pages.Startup
 {
+    /// <summary>
+    /// Tests for the Startup.cs page defined in this class
+    /// </summary>
     public class StartupTests
     {
         #region TestSetup
 
+     
         [SetUp]
         public void TestInitialize()
         {
@@ -16,11 +20,19 @@ namespace UnitTests.Pages.Startup
 
         public class Startup : CupOfSugar.WebSite.Startup
         {
+            /// <summary>
+            /// Constructor to Startup 
+            /// </summary>
+            /// <param name="config"></param>
             public Startup(IConfiguration config) : base(config) { }
         }
         #endregion TestSetup
 
         #region ConfigureServices
+
+        /// <summary>
+        /// Unit test to verify if the services are working 
+        /// </summary>
         [Test]
         public void Startup_ConfigureServices_Valid_Defaut_Should_Pass()
         {
@@ -30,6 +42,10 @@ namespace UnitTests.Pages.Startup
         #endregion ConfigureServices
 
         #region Configure
+
+        /// <summary>
+        /// Unit test to verify if the website is building correctly
+        /// </summary>
         [Test]
         public void Startup_Configure_Valid_Defaut_Should_Pass()
         {
