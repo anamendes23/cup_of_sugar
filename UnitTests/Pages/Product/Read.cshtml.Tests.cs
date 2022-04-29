9f3d28672470b886001194edff7da001173c87fb
anamendes23 <anamendes23@gmail.com>
2022-04-28 20:50:20 -0700

    Added an empty unit test file for future Read page tests as Read.cshtml.Tests.cs

---
+++ UnitTests/Pages/Product/Read.cshtml.Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CupOfSugar.Pages.Product;
+
+namespace UnitTests.Pages.Product
+{
+    /// <summary>
+    ///  Unit tests class for Read page
+    /// </summary>
+    public class Read
+    {
+        /// This class is a place holder
+        /// Tests will be added as Read.cshtml is implemented
+    }
+}
