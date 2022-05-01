68e400e8351f65a4f701c496d69a06304c2b8b3b
anamendes23 <anamendes23@gmail.com>
2022-05-01 13:57:45 -0700

    Adding empty unit test class for Delete page in Delete.cshtml.Tests.cs

---
+++ UnitTests/Pages/Product/Delete.cshtml.Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Pages.Product
+{
+    /// <summary>
+    ///  Unit tests class for Read page
+    /// </summary>
+    public class Delete
+    {
+        /// This class is a place holder
+        /// Tests will be added as Delete.cshtml is implemented
+    }
+}
