e2a348eca1259e5b4fcdce12a23269b83464f23d
Arielle Wilson <jwarielle@gmail.com>
2022-04-30 18:54:55 -0700

    Added dependency classes to Lend.cshtml.Tests.cs

--- UnitTests/Pages/Lend.cshtml.Tests.cs
+++ UnitTests/Pages/Lend.cshtml.Tests.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using CupOfSugar.Pages;
 
 namespace UnitTests.Pages
 {
