2c11af93b1328b1f45f0cbffca42152ec6f699f8
anamendes23 <anamendes23@gmail.com>
2022-05-01 14:00:05 -0700

    Adding Lending.cshtml.Tests.cs to Pages/Product folder

---
+++ UnitTests/Pages/Product/Lend.cshtml.Tests.cs
@@ -0,0 +1,118 @@
+﻿using System.Linq;
+
+using NUnit.Framework;
+
+using CupOfSugar.Pages.Product;
+using CupOfSugar.Pages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTests.Pages.Product
+{
+    /// <summary>
+    /// Unit test for Lend page
+    /// </summary>
+    public class LendTests
+    {
+        #region TestSetup
+        public static LendModel pageModel; // Instance of LendModel
+
+        /// <summary>
+        /// Initializing method that creates an object of the LendModel
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new LendModel(TestHelper.ProductService)
+        {
+        };
+    }
+
+        #endregion TestSetup
+
+        #region OnGet
+
+        /// <summary>
+        /// The Unit test case to check if the correct HTTP request ID is being retrieved
+        /// And also check if its valid or not
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+            var oldCount = TestHelper.ProductService.GetProducts().Count();
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(oldCount + 1, TestHelper.ProductService.GetProducts().Count());
+        }
+
+        #endregion OnGet
+
+        #region OnPost
+
+        /// <summary>
+        /// The Unit test case to check if a Product is returned 
+        /// When form is posted, and redirect to correct page
+        /// </summary>
+        [Test]
+        public void OnPostAsync_Valid_Should_Return_Products()
+        {
+            // Arrange
+            pageModel.Product = new CupOfSugar.WebSite.Models.Product
+            {
+                Id = "bogus",
+                Lender = "bogus",
+                Image = "bogus",
+                Title = "bogus",
+                Address = "bogus",
+                Phone = "bogus",
+                Quantity = "bogus",
+                Category = "bogus",
+                Status = "bogus"
+            };
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, result.PageName.Contains("Index"));
+        }
+
+        /// <summary>
+        /// The Unit test case to check if an invalid Lendmodel
+        /// Causes a invalid return page
+        /// </summary>
+        [Test]
+        public void OnPostAsync_InValid_Model_NotValid_Return_Page()
+        {
+            // Arrange
+            pageModel.Product = new CupOfSugar.WebSite.Models.Product()
+            {
+                Id = "bogus",
+                Lender = "bogus",
+                Image = "bogus",
+                Title = "bogus",
+                Address = "bogus",
+                Phone = "bogus",
+                Quantity = "bogus",
+                Category = "bogus",
+                Status = "bogus"
+            };
+
+            // Force an invalid error state
+            pageModel.ModelState.AddModelError("bogus", "bogus error");
+
+            // Act
+            var result = pageModel.OnPost() as ActionResult;
+
+            // Assert
+            Assert.AreEqual(false, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnPost
+    }
+}
