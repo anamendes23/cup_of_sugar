91f3e9ec783a42dd2d368c977c024975850336f8
Arielle Wilson <jwarielle@gmail.com>
2022-04-30 18:45:16 -0700

    Added class and method documentation for TestInitialize, UpdateData_Valid_Product_Should_Return_Updated_Product, and CreateData_Valid_Product_Should_Return_New_Product methods to JsonFileProductServiceTests.cs

--- UnitTests/Services/JsonFileProductServiceTests.cs
+++ UnitTests/Services/JsonFileProductServiceTests.cs
@@ -10,10 +10,17 @@ using System;
 
 namespace UnitTests.Pages.Product.AddRating
 {
+    /// <summary>
+    /// JsonFileProductServiceTests unit test class
+    /// with tests for the JsonFileProductService
+    /// </summary>
     public class JsonFileProductServiceTests
     {
         #region TestSetup
 
+        /// <summary>
+        /// Initialize test
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -23,22 +30,28 @@ namespace UnitTests.Pages.Product.AddRating
 
         #region UpdateData
 
+        /// <summary>
+        /// Test that passes if UpdateData returns an updated product 
+        /// when a valid product is passed to this method.
+        /// Returned Updated object should have same fields as valid
+        /// object passed to UpdateData.
+        /// </summary>
         [Test]
         public void UpdateData_Valid_Product_Should_Return_Updated_Product()
         {
             // Arrange
-            var productData = TestHelper.ProductService.CreateData();
-            var updateLenderString = "Test Lender";
-            productData.Lender = updateLenderString;
+            var productData = TestHelper.ProductService.CreateData(); // Create new product
+            var updateLenderString = "Test Lender"; 
+            productData.Lender = updateLenderString; // Set new product to have lender of "Test Lender"
 
             // Act
             var result = TestHelper.ProductService.UpdateData(productData);
             var testLender = TestHelper.ProductService.GetProducts().First(x => x.Id == productData.Id).Lender;
 
             // Assert
-            Assert.AreNotEqual(null, result);
-            Assert.AreEqual(productData.Id, result.Id);
-            Assert.AreEqual(testLender, updateLenderString);
+            Assert.AreNotEqual(null, result); // Updated object should not be null
+            Assert.AreEqual(productData.Id, result.Id); // Passed in object and updted object should have equal Ids
+            Assert.AreEqual(testLender, updateLenderString); // Updated object Lender should equal "Test lender" 
         }
 
         [Test]
@@ -59,6 +72,10 @@ namespace UnitTests.Pages.Product.AddRating
 
         #region CreateData
 
+        /// <summary>
+        /// Passes if CreateData method successfully creates a 
+        /// non-null Product
+        /// </summary>
         [Test]
         public void CreateData_Valid_Product_Should_Return_New_Product()
         {
