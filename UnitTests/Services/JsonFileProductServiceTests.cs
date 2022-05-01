dc34821cb51509e40c4627c1eb0bce7b53bd24dd
Arielle Wilson <jwarielle@gmail.com>
2022-04-30 17:11:09 -0700

    Fixed CupOfSugar references and deleted AddRatings region to JsonFileProductServiceTests.cs

--- UnitTests/Services/JsonFileProductServiceTests.cs
+++ UnitTests/Services/JsonFileProductServiceTests.cs
@@ -3,8 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 
 using NUnit.Framework;
 
-using ContosoCrafts.WebSite.Pages.Product;
-using ContosoCrafts.WebSite.Models;
+using CupOfSugar.Pages;
+using CupOfSugar.WebSite.Models;
 using System.Linq;
 using System;
 
@@ -21,143 +21,6 @@ namespace UnitTests.Pages.Product.AddRating
 
         #endregion TestSetup
 
-        #region AddRating
-        [Test]
-        public void AddRating_InValid_Product_Null_Should_Return_False()
-        {
-            // Arrange
-
-            // Act
-            var result = TestHelper.ProductService.AddRating(null, 1);
-
-            // Assert
-            Assert.AreEqual(false, result);
-        }
-
-        [Test]
-        public void AddRating_InValid_()
-        {
-            // Arrange
-
-            // Act
-            var result = TestHelper.ProductService.AddRating(null, 1);
-
-            // Assert
-            Assert.AreEqual(false, result);
-        }
-
-        // ....
-
-        [Test]
-        public void AddRating_Valid_Product_Valid_Rating_Valid_Should_Return_True()
-        {
-            // Arrange
-
-            // Get the First data item
-            var data = TestHelper.ProductService.GetAllData().First();
-            var countOriginal = data.Ratings.Length;
-
-            // Act
-            var result = TestHelper.ProductService.AddRating(data.Id, 5);
-            var dataNewList = TestHelper.ProductService.GetAllData().First();
-
-            // Assert
-            Assert.AreEqual(true, result);
-            Assert.AreEqual(countOriginal + 1, dataNewList.Ratings.Length);
-            Assert.AreEqual(5, dataNewList.Ratings.Last());
-        }
-
-        // ....
-
-        [Test]
-        public void AddRating_InValid_Product_Should_Return_False()
-        {
-            // Arrange
-
-            // Act
-            var result = TestHelper.ProductService.AddRating("95", 1);
-            
-            // Assert
-            Assert.AreEqual(false, result);
-
-        }
-
-        [Test]
-        public void AddRating_Product_Not_Found_Return_False()
-        {
-            // Arrange
-
-            // Act
-            var result = TestHelper.ProductService.AddRating("95", 1);
-
-            // Assert
-            Assert.AreEqual(false, result);
-
-        }
-
-        [Test]
-        public void AddRating_Invalid_Rating_High()
-        {
-            // Arrange
-
-            // Get the First data item
-            var data = TestHelper.ProductService.GetAllData().First();
-
-            // Act
-            var result = TestHelper.ProductService.AddRating(data.Id, 6);
-
-            // Assert
-            Assert.AreEqual(false, result);
-        }
-
-        [Test]
-        public void AddRating_Invalid_Rating_Low()
-        {
-            // Arrange
-
-            // Get the First data item
-            var data = TestHelper.ProductService.GetAllData().First();
-
-            // Act
-            var result = TestHelper.ProductService.AddRating(data.Id, -1);
-
-            // Assert
-            Assert.AreEqual(false, result);
-        }
-
-        [Test]
-        public void Ratings_Empty_Return_True()
-        {
-            // Arrange
-
-            // Get the First data item
-            var data = TestHelper.ProductService.GetAllData().FirstOrDefault((product) => product.Ratings == null);
-            // if data is null, create a new product that has rating empty
-            if (data == null)
-            {
-                data = TestHelper.ProductService.CreateData();
-            }
-            
-            // Act
-            var result = TestHelper.ProductService.AddRating(data.Id, 1);
-            // Assert
-            Assert.AreEqual(true, result);
-        }
-
-        [Test]
-        public void Update_Data_Return_Null()
-        {
-            // Arrange
-
-            // Get the First data item
-            ProductModel model = new ProductModel();
-            model.Id = "95";
-            // Act
-            var result = TestHelper.ProductService.UpdateData(model);
-            // Assert
-            Assert.AreEqual(null, result);
-        }
-
-        #endregion AddRating
+       
     }
 }
