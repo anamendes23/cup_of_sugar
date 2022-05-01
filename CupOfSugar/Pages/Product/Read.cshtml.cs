2e6b0658551eb592865b43d3b7ac05a8b7d0479a
anamendes23 <anamendes23@gmail.com>
2022-05-01 11:38:07 -0700

    Implemented ReadModel in Read.cshtml.cs

--- CupOfSugar/Pages/Product/Read.cshtml.cs
+++ CupOfSugar/Pages/Product/Read.cshtml.cs
@@ -4,13 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CupOfSugar.WebSite.Models;
+using CupOfSugar.WebSite.Services;
 
 namespace CupOfSugar.Pages.Product
 {
     public class ReadModel : PageModel
     {
-        public void OnGet()
+        // Data middletier
+        public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
+        public ReadModel(JsonFileProductService productService)
         {
+            ProductService = productService;
+        }
+
+        // The data to show
+        public CupOfSugar.WebSite.Models.Product Product;
+
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
         }
     }
 }
