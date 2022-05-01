a2fae0a8442eefefbad46ef79f48e696607c3c9f
anamendes23 <anamendes23@gmail.com>
2022-05-01 14:11:08 -0700

    Added initial implementation of DataModel in Delete.cshtml.cs

--- CupOfSugar/Pages/Product/Delete.cshtml.cs
+++ CupOfSugar/Pages/Product/Delete.cshtml.cs
@@ -4,13 +4,57 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CupOfSugar.WebSite.Services;
+using CupOfSugar.WebSite.Models;
 
 namespace CupOfSugar.Pages.Product
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        // Data middletier
+        public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
+        public DeleteModel(JsonFileProductService productService)
         {
+            ProductService = productService;
+        }
+
+        // The data to show, bind to it for the post
+        [BindProperty]
+        public CupOfSugar.WebSite.Models.Product Product { get; set; }
+
+        /// <summary>
+        /// REST Get request
+        /// Loads the Data
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
+        }
+
+        /// <summary>
+        /// Post the model back to the page
+        /// The model is in the class variable Product
+        /// Call the data layer to Delete that data
+        /// Then return to the index page
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            ProductService.DeleteData(Product.Id);
+
+            return RedirectToPage("./Borrow");
         }
     }
 }
