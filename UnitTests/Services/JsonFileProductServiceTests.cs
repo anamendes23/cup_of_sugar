f577c0e2ea4373ab6f2249ec69263aab65a88179
Arielle Wilson <jwarielle@gmail.com>
2022-04-30 17:41:17 -0700

    Added UpdateData_Valid_Product_Should_Return_Updated_Product test to JsonFileProductServiceTest.cs

--- UnitTests/Services/JsonFileProductServiceTests.cs
+++ UnitTests/Services/JsonFileProductServiceTests.cs
@@ -21,6 +21,27 @@ namespace UnitTests.Pages.Product.AddRating
 
         #endregion TestSetup
 
-       
+        #region UpdateData
+
+        [Test]
+        public void UpdateData_Valid_Product_Should_Return_Updated_Product()
+        {
+            // Arrange
+            // Get the First data item
+            var productData = TestHelper.ProductService.CreateData();
+            var updateLenderString = "Test Lender";
+            productData.Lender = updateLenderString;
+
+            // Act
+            var result = TestHelper.ProductService.UpdateData(productData);
+            var testLender = TestHelper.ProductService.GetProducts().First(x => x.Id == productData.Id).Lender;
+
+            // Assert
+            Assert.AreNotEqual(null, result);
+            Assert.AreEqual(productData.Id, result.Id);
+            Assert.AreEqual(testLender, updateLenderString);
+        }
+
+        #endregion UpdateData
     }
 }
