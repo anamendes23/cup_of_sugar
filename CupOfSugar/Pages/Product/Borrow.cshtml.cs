3ffb31e94dd2e91a826b18ddd19a34934b755309
anamendes23 <anamendes23@gmail.com>
2022-05-01 11:35:08 -0700

    Implemented BorrowModel in Borrow.cshtml.cs

--- CupOfSugar/Pages/Product/Borrow.cshtml.cs
+++ CupOfSugar/Pages/Product/Borrow.cshtml.cs
@@ -5,7 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using CupOfSugar.WebSite.Models;
+using CupOfSugar.WebSite.Services;
 /// <summary>
 /// Borrow (Index) page
 /// Will use get method to retrieve the food items
@@ -14,9 +15,27 @@ namespace CupOfSugar.Pages
 {
     public class BorrowModel : PageModel
     {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService"></param>
+        public BorrowModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // Data Service
+        public JsonFileProductService ProductService { get; }
+
+        // Collection of the Data
+        public IEnumerable<CupOfSugar.WebSite.Models.Product> Products { get; private set; }
+
+        /// <summary>
+        /// REST OnGet, return all data
+        /// </summary>
         public void OnGet()
         {
-
+            Products = ProductService.GetProducts();
         }
     }
 }
