af5b1aa985c3b129c222ee2987d1cc3d266c17e5
Arielle Wilson <jwarielle@gmail.com>
2022-05-08 19:13:51 -0700

    Updated Quantity to int and removed Required annotation to Product.cs

--- CupOfSugar/Models/Product.cs
+++ CupOfSugar/Models/Product.cs
@@ -18,29 +18,22 @@ namespace CupOfSugar.WebSite.Models
         public string Id { get; set; } //string Id of product
 
         [StringLength(60, MinimumLength = 3)]
-        [Required]
         public string Lender { get; set; } //string Lender of product
 
         [JsonPropertyName("img")]
-        [Required]
         public string Image { get; set; } //string url of Image of the product
 
         [StringLength(60, MinimumLength = 3)]
-        [Required]
         public string Title { get; set; } //string Title of product
 
-        [Required]
         public string Address { get; set; } //string Adress of Lender
 
         [DataType(DataType.PhoneNumber)]
         [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
-        [Required]
         public string Phone { get; set; } //string phone of Lender
 
-        [Required]
-        public string Quantity { get; set; } //string quantity of product
+        public int Quantity { get; set; } //int quantity of product
 
-        [Required]
         public string Category { get; set; } // string category of product
 
         public string Status { get; set; } // string status of product
