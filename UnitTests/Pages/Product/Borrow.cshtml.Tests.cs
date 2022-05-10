cb39c1b8bdff2221b81fe87c2ee20ebb23f2eee2
Ananya Reddy Komatireddy <ananyareddy2435@gmail.com>
2022-05-10 01:56:10 -0700

    Added Unit Tests with 100% code coverage for the Borrow.cshtml.cs page to Borrow.cshtml.Tests.cs

---
+++ UnitTests/Pages/Product/Borrow.cshtml.Tests.cs
@@ -0,0 +1,59 @@
+﻿using CupOfSugar.Pages;
+using NUnit.Framework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Pages.Product
+{
+    /// <summary>
+    /// Unit tests for borrow/index page
+    /// </summary>
+    public class Borrow
+    {
+        #region TestSetup
+
+
+        // Instance of LendModel
+        public static BorrowModel pageModel;
+
+        /// <summary>
+        /// Initializing method that creates an 
+        /// object of the BorrowModel
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new BorrowModel(TestHelper.ProductService) { };
+        }
+
+        #endregion
+
+        #region OnGet
+
+        /// <summary>
+        /// The unit test to check if the correct 
+        /// HTTP request ID is being retrieved
+        /// And also check if its valid or not
+        /// And also check if all the products are retrieved
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, pageModel.Products.ToList().Any());
+        }
+
+        #endregion OnGet
+
+    }
+}
