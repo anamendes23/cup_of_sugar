e235b8d212243ecf9956580516dbf6d1b9a82a2a
anamendes23 <anamendes23@gmail.com>
2022-05-14 15:06:50 -0700

    Removed unnecessary using directives in Update.cshtml.cs

--- CupOfSugar/Pages/Product/Update.cshtml.cs
+++ CupOfSugar/Pages/Product/Update.cshtml.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using CupOfSugar.WebSite.Services;
-using CupOfSugar.WebSite.Models;
 
 namespace CupOfSugar.Pages.Product
 {
