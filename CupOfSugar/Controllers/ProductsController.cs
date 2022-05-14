8539cb82659d518c56400f1772a636caa9ec9ce7
anamendes23 <anamendes23@gmail.com>
2022-05-14 15:04:52 -0700

    Removed unnecessary using directives in ProductsController.cs

--- CupOfSugar/Controllers/ProductsController.cs
+++ CupOfSugar/Controllers/ProductsController.cs
@@ -1,9 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CupOfSugar.WebSite.Services;
 using CupOfSugar.WebSite.Models;
 
