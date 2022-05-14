ebc24dc44f4749d5ab9641fddd6e76109c30bb63
anamendes23 <anamendes23@gmail.com>
2022-05-14 14:56:57 -0700

    Removed unnecessary using directives in Delete.cshtml.Tests.cs

--- UnitTests/Pages/Product/Delete.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Delete.cshtml.Tests.cs
@@ -1,13 +1,7 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
 using CupOfSugar.Pages.Product;
-using CupOfSugar.WebSite.Models;
 
 namespace UnitTests.Pages.Product
 {
