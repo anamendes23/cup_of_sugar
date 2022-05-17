fe6461065c5b3aef695dec11f1b28d632d4f22b7
Ananya Reddy Komatireddy <ananyareddy2435@gmail.com>
2022-05-17 00:12:36 -0700

    Added an event listener to enable dark mode to site.js

--- CupOfSugar/wwwroot/js/site.js
+++ CupOfSugar/wwwroot/js/site.js
@@ -1,4 +1,2 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your Javascript code.
+﻿//code to enable the dark mode when the dark mode button on the navbar is toggled
+var darkSwitch = document.getElementById("darkSwitch"); window.addEventListener("load", (function () { if (darkSwitch) { initTheme(); darkSwitch.addEventListener("change", (function () { resetTheme() })) } })); function initTheme() { var darkThemeSelected = localStorage.getItem("darkSwitch") !== null && localStorage.getItem("darkSwitch") === "dark"; darkSwitch.checked = darkThemeSelected; darkThemeSelected ? document.body.setAttribute("data-theme", "dark") : document.body.removeAttribute("data-theme") } function resetTheme() { if (darkSwitch.checked) { document.body.setAttribute("data-theme", "dark"); localStorage.setItem("darkSwitch", "dark") } else { document.body.removeAttribute("data-theme"); localStorage.removeItem("darkSwitch") } }
