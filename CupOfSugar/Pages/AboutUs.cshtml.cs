92fbd77bb953010a90b4081bcf7b193678c472d9
anamendes23 <anamendes23@gmail.com>
2022-05-17 10:16:15 -0700

    Added AboutUsModel constructor and comments in AboutUs.cshtml.cs

--- CupOfSugar/Pages/AboutUs.cshtml.cs
+++ CupOfSugar/Pages/AboutUs.cshtml.cs
@@ -1,14 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace CupOfSugar.Pages
 {
     public class AboutUsModel : PageModel
     {
+        private readonly ILogger<AboutUsModel> _logger; //readonly ILogger object that allows logging
+
+        /// <summary>
+        /// AboutUsModel constructor
+        /// initializes logger
+        /// </summary>
+        /// <param name="logger"></param>
+        public AboutUsModel(ILogger<AboutUsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// placeholder for OnGet method
+        /// </summary>
         public void OnGet()
         {
         }
