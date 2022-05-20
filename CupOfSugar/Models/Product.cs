573ce9070427bc2c71079be9ecd7873d99be709c
Arielle Wilson <jwarielle@gmail.com>
2022-05-20 08:41:14 -0700

    Made all attributes required and modified error messages to Product.cs

--- CupOfSugar/Models/Product.cs
+++ CupOfSugar/Models/Product.cs
@@ -14,23 +14,30 @@ namespace CupOfSugar.WebSite.Models
 
         public string Id { get; set; } //string Id of product
 
+        [Required(ErrorMessage = "Please enter your name.")]
         [StringLength(60, MinimumLength = 3)]
         public string Lender { get; set; } //string Lender of product
 
+        [Required(ErrorMessage = "Please enter an image URL.")]
+        [Url(ErrorMessage = "Please enter a valid image URL")]
         [JsonPropertyName("img")]
         public string Image { get; set; } //string url of Image of the product
 
-        [StringLength(60, MinimumLength = 3)]
+        [Required(ErrorMessage = "Please enter a title between 3 and 60 characters.")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "Please enter a title between 3 and 60 characters.")]
         public string Title { get; set; } //string Title of product
 
+        [Required(ErrorMessage = "Please enter your address.")]
         public string Address { get; set; } //string Adress of Lender
 
+        [Required(ErrorMessage = "Please enter a 10 digit phone number.")]
         [DataType(DataType.PhoneNumber)]
         [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", 
             ErrorMessage = "Not a valid phone number. Please enter a 10 digit phone number.")]
         public string Phone { get; set; } //string phone of Lender
 
-        [Range(1, Int32.MaxValue, ErrorMessage = "Value should be greater than or equal to 1")]
+        [Required(ErrorMessage = "Please enter a quantity greater than 0.")]
+        [Range(1, Int32.MaxValue, ErrorMessage = "Please enter a quantity greater than 0.")]
         public int Quantity { get; set; } //int quantity of product
 
         [Required(ErrorMessage = "Please select a category from the list. " +
