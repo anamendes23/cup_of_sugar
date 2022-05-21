78d8d3a0ddc58619fd2c16795c33886131be41f1
anamendes23 <anamendes23@gmail.com>
2022-05-20 22:17:57 -0700

    Formatted dark mode script in site.js

--- CupOfSugar/wwwroot/js/site.js
+++ CupOfSugar/wwwroot/js/site.js
@@ -1,2 +1,26 @@
 ﻿//code to enable the dark mode when the dark mode button on the navbar is toggled
-var darkSwitch = document.getElementById("darkSwitch"); window.addEventListener("load", (function () { if (darkSwitch) { initTheme(); darkSwitch.addEventListener("change", (function () { resetTheme() })) } })); function initTheme() { var darkThemeSelected = localStorage.getItem("darkSwitch") !== null && localStorage.getItem("darkSwitch") === "dark"; darkSwitch.checked = darkThemeSelected; darkThemeSelected ? document.body.setAttribute("data-theme", "dark") : document.body.removeAttribute("data-theme") } function resetTheme() { if (darkSwitch.checked) { document.body.setAttribute("data-theme", "dark"); localStorage.setItem("darkSwitch", "dark") } else { document.body.removeAttribute("data-theme"); localStorage.removeItem("darkSwitch") } }
+var darkSwitch = document.getElementById("darkSwitch");
+window.addEventListener("load", (function () {
+    if (darkSwitch) {
+        initTheme();
+        darkSwitch.addEventListener("change", (function () {
+            resetTheme()
+        }))
+    }
+}));
+
+function initTheme() {
+    var darkThemeSelected = localStorage.getItem("darkSwitch") !== null && localStorage.getItem("darkSwitch") === "dark";
+    darkSwitch.checked = darkThemeSelected;
+    darkThemeSelected ? document.body.setAttribute("data-theme", "dark") : document.body.removeAttribute("data-theme")
+}
+
+function resetTheme() {
+    if (darkSwitch.checked) {
+        document.body.setAttribute("data-theme", "dark");
+        localStorage.setItem("darkSwitch", "dark")
+    } else {
+        document.body.removeAttribute("data-theme");
+        localStorage.removeItem("darkSwitch")
+    }
+}
