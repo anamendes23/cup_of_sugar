19ca40c969f797701fc84c787e5ca59a72c9b45c
Arielle Wilson <jwarielle@gmail.com>
2022-05-23 00:19:00 -0700

    Added test helper class for BunitTestContext to TestBUnitHelper.cs

---
+++ UnitTests/TestBUnitHelper.cs
@@ -0,0 +1,19 @@
+﻿using Bunit;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// Test Context used by bUnit
+    /// </summary>
+    public abstract class BunitTestContext : TestContextWrapper
+    {
+        // The Setup sets the context
+        [SetUp]
+        public void Setup() => TestContext = new Bunit.TestContext();
+
+        // When done displose removes it, to free up system resources
+        [TearDown]
+        public void TearDown() => TestContext.Dispose();
+    }
+}
